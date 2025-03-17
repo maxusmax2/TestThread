688ab5d44c5bfdd4014885ea1ddbb6efc8a5e5ea
Maksim Karpov <maxusmax2@gmail.com>
2025-03-17 19:41:48 +0300

    Add my thread pool

---
+++ MyThreadPool.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Concurrent;
+
+namespace TestThread;
+
+public class MyThreadPool
+{
+    private readonly ConcurrentQueue<ActionWrapper> _workQueue;
+    private readonly CancellationToken _ct;
+    private readonly Thread _workThread;
+    public MyThreadPool(CancellationToken cancellationToken = default)
+    {
+        _workQueue = new ConcurrentQueue<ActionWrapper>();
+        _ct = cancellationToken;
+        _workThread = new Thread(Worker);
+        _workThread.Start();
+    }
+
+    public void AddAction(Action action, CancellationToken cancellationToken = default)
+    {
+        _workQueue.Enqueue(new ActionWrapper(action, cancellationToken));
+    }
+
+    private void Worker()
+    {
+        while (!_ct.IsCancellationRequested)
+        {
+            if (_workQueue.TryDequeue(out var task))
+            {
+                if (!task.CancellationToken.IsCancellationRequested) 
+                {
+                    task.Action.Invoke();
+                }
+            }
+            else
+            {
+                Thread.Sleep(1);
+                continue;
+            }
+        }
+    }
+
+    private class ActionWrapper
+    {
+        public Action Action { get; init; }
+        public Guid CallbackId { get; init; }
+        public CancellationToken CancellationToken { get; init; }
+
+        public ActionWrapper(Action action, CancellationToken cancellationToken)
+        {
+            Action = action;
+            CallbackId = Guid.NewGuid();
+            CancellationToken = cancellationToken;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using TestThread;
+
+var cts = new CancellationTokenSource();
+var ct = cts.Token;
+var threadPool = new MyThreadPool();
+
+for (var i = 0; i < 100; i++)
+{
+    threadPool.AddAction(() => { 
+        i++;
+        Console.WriteLine(i);
+        Thread.Sleep(1);
+    }, cts.Token);
+}
+Console.WriteLine("Hello World!");
+Thread.Sleep(1000);
+cts.Cancel();
+
+Console.ReadLine();
