5cc861b3db26ab2abe86511c9517f54cd6e19cf7
Maksim Karpov <maksim.karpov@altenar.com>
2025-05-11 16:57:17 +0300

    Add helper

---
+++ Helper.cs
@@ -0,0 +1,31 @@
+﻿namespace TestThread
+{
+    public static class Helper
+    {
+        public static Task<TResult> WithCancellation<TResult>(this Task<TResult> task, CancellationToken ct)
+        {
+            return task.WaitAsync(ct);
+        }
+
+
+        public static async Task<TResult[]> WhenAllOrError<TResult>(params Task<TResult>[] tasks)
+        {
+            var taskList = tasks.ToList();
+            var completedTasks = new List<Task>();
+
+            while (taskList.Any())
+            {
+                var completedTask = await Task.WhenAny(taskList);
+                taskList.Remove(completedTask);
+                completedTasks.Add(completedTask);
+
+                if (completedTask.IsFaulted)
+                {
+                    throw completedTask.Exception?.Flatten();
+                }
+            }
+
+            return await Task.WhenAll(tasks);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,5 @@
-﻿using static System.Console;
-using TestThread;
-class Program
-{
-    static async Task Main(string[] args)
-    {
-        Console.WriteLine(await (await await false && await true));
+﻿
 
-        MyAwaiter me = new MyAwaiter();
-        WriteLine($"Run {Thread.CurrentThread.ManagedThreadId}");
-        _ = Task.Run(() => 
-        {
-            Thread.Sleep(1000);
-            WriteLine($"SetCompleted {Thread.CurrentThread.ManagedThreadId}");
-            me.SetCompeleted();
-        });
-        await me;
-        WriteLine($"After await {Thread.CurrentThread.ManagedThreadId}");
-        ReadLine();
-    }
-}
+var task = new Task(()=> Console.WriteLine("Test123"));
+task.WaitAsync();
+
